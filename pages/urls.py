486cd903c0419d0f51631130f58a902d7d1f30f5
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-28 16:58:11 +0300

    add pages info

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, contact_view
 
 urlpatterns = [
     path('', index_view, name="index_view"),
+    path('contact/', contact_view, name="index_view"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'pages/index.html', {})
+
+
+def contact_view(request):
+    return render(request, 'pages/contact.html', {})
